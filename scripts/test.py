ec3821adab6d24c5f43b973d18edcec261747770
hize00 <carlo.fanton92@gmail.com>
2017-05-06 16:11:36 +0200

    small structural changes

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,9 @@
+#######TIPO DI COMMENTO#######
+
+""" COMMENTO A FUNZIONE"""
+### codice da implementare
+# commento a riga di codice
+
 from copy import deepcopy
 
 #######OPERAZIONI STUPIDE SU DIZIONARI#########
@@ -34,53 +40,51 @@ temporaryConfigurations = {}
 #list of timestamp and correlated configurations
 OUTPUT = {}
 
-### R(C): ritorna i robots presenti in configurazione C
+""" R(C): ritorna i robots presenti in configurazione C in ordine di ID """
 def robotsInConfiguration(configuration):
 	return sorted(configuration.keys())
 #print "\nC0 has ", robotsInConfiguration(C0)
 
-### C(r): vertex in which robot r is in configuration C. Se r non e' presente ritorna None 
+""" C(r): vertex in which robot r is in configuration C. Se r non e' presente ritorna None """
 def robotVertex(robot,configuration):
 	if robot in configuration.keys():
 		return configuration.get(robot)
 
 
-### adiacenza tra due vertici
+""" adiacenza tra due vertici """
 def adjacency(vertex1, vertex2):
-	#if vertex1 directly connected to vertex 2:
-		#return 1
-	#else: return 0
+	###if vertex1 directly connected to vertex 2:
+		###return 1
+	###else: return 0
 
-#move robot in an adjacent vertex
+""" move robot in an adjacent vertex """
 def adjacentMove(robot,configuration,newVertex):
-	#start time
 	newConfiguration = deepcopy(configuration)
 	newConfiguration.update({robot:newVertex})
-	#quando robot a destinazione -> end time
 	return newConfiguration	#e tempo
 
 
-#move robot from configuration (if exists) to newVertex in newConfiguration
+""" move robot from configuration (if exists) to newVertex in newConfiguration """
 def moveRobot(robot,configuration,newVertex):
 	if robot not in configuration.keys():
 		print "Error: " + str(robot) + " not in configuration "+str(configuration)
+		return configuration
 	else:
+		###start time
+
 		if (adjacency(robotVertex(robot,configuration),newVertex)==1):	#if robot is in a vertex adjacent to newVertex it moves there directly
 			newConfiguration=adjacentMove(robot,configuration,newVertex)
 		else:
-			#compute SHORTEST PATH between the two vertexes. Return a list of nodes
-			#path_nodes = shortestPath(robotVertex(robot,configuration), newVertex)
+			###compute SHORTEST PATH between the two vertexes. Return a list of nodes
+			###path_nodes = shortestPath(robotVertex(robot,configuration), newVertex)
 
 			#move robot in intermediate configurations (each step is an adjacent move)
-			for i in path_nodes:
+			for i in range(1,len(path_nodes)):
 				#adjacentMove from robotVertex and first node on the list
-				adjacentMove(robotVertex(robot,configuration),path_nodes[i])
+				temporaryConfigurations[i] = adjacentMove(robotVertex(robot,configuration),path_nodes[i])
 				#adjusting indexes
 				robotVertex(robot,configuration) = path_nodes[i]
 
 
-
-		
-
-		return #OUTPUT(i) quindi timestamp e configurazione
+		return ###OUTPUT(i) quindi timestamp e configurazione
 
