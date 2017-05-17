e7d9732d496e53a8a8751503ce079ac32cc3e32e
hize00 <carlo.fanton92@gmail.com>
2017-05-17 14:52:36 +0200

    modifche

--- scripts/kumar t&d.py
+++ scripts/kumar t&d.py
@@ -1,25 +1,63 @@
 import math
 import numpy as np
 
-
 """
-IMPLEMENTATION OF KUMAR ALGORITHM - 2 ROBOT CASE
-The maps are taken from the paper 'CONSTRUCTING RADIO SIGNAL STRENGTH MAPS WITH MULTIPLE ROBOT'
+#GRAPH BUILT PREVIOUSLY
+#robot velocity = 2.3 m/s
 
+
+#popolamento manuale matrice distanze
+distance_matrix =  np.zeros((N_VERTEXES, N_VERTEXES))
+distance_matrix[0][0] = 0
+distance_matrix[0][1] = 14
+distance_matrix[0][2] = 27
+distance_matrix[0][3] = 35
+distance_matrix[0][4] = 40
+distance_matrix[1][0] = 14
+distance_matrix[1][1] = 0
+distance_matrix[1][2] = 17
+distance_matrix[1][3] = 24
+distance_matrix[1][4] = 42
+distance_matrix[2][0] = 27
+distance_matrix[2][1] = 17
+distance_matrix[2][2] = 0
+distance_matrix[2][3] = 21
+distance_matrix[2][4] = 38
+distance_matrix[3][0] = 27
+distance_matrix[3][1] = 24
+distance_matrix[3][2] = 21
+distance_matrix[3][3] = 0
+distance_matrix[3][4] = 19
+distance_matrix[4][0] = 40
+distance_matrix[4][1] = 42
+distance_matrix[4][2] = 38
+distance_matrix[4][3] = 19
+distance_matrix[4][4] = 0
+
+#popolamento matrice tempo
+time_matrix =  np.zeros((N_VERTEXES, N_VERTEXES))
+for i in range(0,N_VERTEXES):
+	for j in range(0,N_VERTEXES):
+		time_matrix[i][j] = distance_matrix[i][j] / 2.3
+
+print "DISTANCE Matrix"
+print distance_matrix
+print "\nTIME Matrix"
+print time_matrix
 """
 
-POINTS_TO_EXPLORE = [(1,5),(2,6),(2,3),(3,4)]
+
+POINTS_TO_EXPLORE = [(1,5),(3,5),(3,4),(1,3),(1,4)]
 N_ROBOT = 2
-N_VERTEXES = 6
+N_VERTEXES = 5
 
 
 #GRAFO DELLA MAPPA
-roadmap_graph = {1: [2],
-         2: [1, 3],
-         3: [2, 4, 5],
-         4: [3],
-         5: [3, 6],
-         6: [5]}
+roadmap_graph = {1: [2,3,4,5],
+         2: [1,3,4,5],
+         3: [1,2,4,5],
+         4: [1,2,3,5],
+         5: [1,2,3,4]}
 
 adjacency_road = np.zeros((N_VERTEXES, N_VERTEXES))
 for i in range (0,N_VERTEXES):
@@ -29,12 +67,11 @@ for i in range (0,N_VERTEXES):
 		
 
 #GRAFO DEI PUNTI IN CUI MISURARE CONNESSIONE
-radiomap_graph = {1: [5],
-         2: [3, 6],
-         3: [2, 4],
-         4: [3],
-         5: [1],
-         6: [2]}
+radiomap_graph = {1: [3, 4, 5],
+         2: [],
+         3: [1, 4, 5],
+         4: [1, 3],
+         5: [1, 3]}
 
 adjacency_radio = np.zeros((N_VERTEXES, N_VERTEXES))
 for i in range (0,N_VERTEXES):
@@ -47,28 +84,6 @@ print adjacency_road
 print"\nRadiomap MATRIX - Pairs to be measured"
 print adjacency_radio
 
-def find_shortest_path(graph, start, end, path=[]):
-        path = path + [start]
-        if start == end:
-            return path
-        if not graph.has_key(start):
-            return None
-        shortest = None
-        for node in graph[start]:
-            if node not in path:
-                newpath = find_shortest_path(graph, node, end, path)
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest
-
-def number_of_moves(graph, vertex1, vertex2):
-	if find_shortest_path(graph, vertex1, vertex2) == None:
-		count = 9999999
-	else : count = len(find_shortest_path(graph, vertex1, vertex2))-1 #subtract 1 otherwise for the movements of 1-2-3-4 we would have a cost of 4 instead of 3
-	return count
-print "\nEX: moves from 1 to 4"
-print number_of_moves(roadmap_graph,1,4)
 
 
 """OPTIMAL PLAN FOR 2 ROBOTS"""
@@ -91,10 +106,10 @@ def cost_graph(roadmap_graph, adjacency_road, radiomap_graph, adjacency_radio):
 	for i in range (0,length):
 		for j in range (0,length):
 			if V2[i][0]!=V2[j][0]:
-				A2[i][j]=1 #eliminabile#
+				A2[i][j]=1
 				#il costo e' il minimo tra somma di (i-i',j-j') OR (i-j',j-i')
-				C2[i][j]=min((number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][0])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][1])) , (number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][1])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][0])))
-
+				#C2[i][j]=min((number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][0])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][1])) , (number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][1])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][0])))
+				
 	print "\n COST MATRIX"
 	#it's symetric wrt diagonal
 	print C2
@@ -108,3 +123,6 @@ il grafo G2 sara' un grafo completamente connesso con tanti vertici quanto la di
 I pesi degli archi sono i valori contenuti nella matrice C
 per trovare il piano ottimo bisogna risolvere il TSP su G2
 """
+
+
+
