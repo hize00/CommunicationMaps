49bbe196180c553318ef2ff0f5065f6a47b89570
hize00 <carlo.fanton92@gmail.com>
2017-05-19 09:18:58 +0200

    2-robot kumar for time and distance

--- scripts/kumar t&d.py
+++ scripts/kumar t&d.py
@@ -1,8 +1,14 @@
 import math
 import numpy as np
 
-"""
-#GRAPH BUILT PREVIOUSLY
+
+
+
+POINTS_TO_EXPLORE = [(1,5),(3,5),(3,4),(1,3),(1,4)]
+N_ROBOT = 2
+N_VERTEXES = 5
+
+#GRAPH BUILT BY ME
 #robot velocity = 2.3 m/s
 
 
@@ -44,15 +50,10 @@ print "DISTANCE Matrix"
 print distance_matrix
 print "\nTIME Matrix"
 print time_matrix
-"""
 
 
-POINTS_TO_EXPLORE = [(1,5),(3,5),(3,4),(1,3),(1,4)]
-N_ROBOT = 2
-N_VERTEXES = 5
 
-
-#GRAFO DELLA MAPPA
+#GRAFO DELLA MAPPA. Grafo completamente connesso
 roadmap_graph = {1: [2,3,4,5],
          2: [1,3,4,5],
          3: [1,2,4,5],
@@ -79,43 +80,60 @@ for i in range (0,N_VERTEXES):
 		if (j+1 in radiomap_graph[i+1]):
 			adjacency_radio[i][j] = 1
 
-print "Roadmap adjacency MATRIX"
+print "\nRoadmap adjacency MATRIX"
 print adjacency_road
 print"\nRadiomap MATRIX - Pairs to be measured"
 print adjacency_radio
 
-
+V2 = []
+z=0
+for i in range (0,N_VERTEXES):
+	for j in range (i,N_VERTEXES):
+		if adjacency_radio[i][j] == 1:
+			z=z+1
+			V2.append((z,[i+1,j+1]))
+print "\n V2: each v represents the links between two points"
+print V2
+	#valori: indice lista / accesso a id_vertice o coppia / quale tra i due punti
+	#print V2[1][1][1] #stampa 4
+length = len(V2)
 
 """OPTIMAL PLAN FOR 2 ROBOTS"""
-def cost_graph(roadmap_graph, adjacency_road, radiomap_graph, adjacency_radio):
-	V2 = []
-	z=0
-	for i in range (0,N_VERTEXES):
-		for j in range (i,N_VERTEXES):
-			if adjacency_radio[i][j] == 1:
-				z=z+1
-				V2.append((z,[i+1,j+1]))
-	print "\n V2: each v represents the links between two points"
-	print V2
-	#valori: indice lista / acesso a id_vertice o coppia / quale tra i due punti
-	#print V2[1][1][1] #stampa 3
-
-	length = len(V2)
+def distance_cost(roadmap_graph, adjacency_road, radiomap_graph, adjacency_radio):
+	
 	A2 = np.zeros((length, length))
-	C2 = np.zeros((length, length))
+	D2 = np.zeros((length, length))
 	for i in range (0,length):
 		for j in range (0,length):
 			if V2[i][0]!=V2[j][0]:
 				A2[i][j]=1
 				#il costo e' il minimo tra somma di (i-i',j-j') OR (i-j',j-i')
-				#C2[i][j]=min((number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][0])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][1])) , (number_of_moves(roadmap_graph,V2[i][1][0],V2[j][1][1])+number_of_moves(roadmap_graph,V2[i][1][1],V2[j][1][0])))
-				
-	print "\n COST MATRIX"
-	#it's symetric wrt diagonal
-	print C2
-	return C2
+				D2[i][j]=min( (distance_matrix[V2[i][1][0]-1][V2[j][1][0]-1] + distance_matrix[V2[i][1][1]-1][V2[j][1][1]-1]) , (distance_matrix[V2[i][1][0]-1][V2[j][1][1]-1]+distance_matrix[V2[i][1][1]-1][V2[j][1][0]-1]))
+	
+	#symmetric wrt diagonal				
+	print "\nCOST MATRIX for DISTANCES"
+	print D2
+	return D2
 
-C = cost_graph(roadmap_graph,adjacency_road,radiomap_graph,adjacency_radio)
+
+def time_cost(roadmap_graph, adjacency_road, radiomap_graph, adjacency_radio):
+	A2 = np.zeros((length, length))
+	T2 = np.zeros((length, length))
+	for i in range (0,length):
+		for j in range (0,length):
+			if V2[i][0]!=V2[j][0]:
+				A2[i][j]=1
+				#il costo e' il minimo tra somma di (i-i',j-j') OR (i-j',j-i')
+				T2[i][j]=min( (time_matrix[V2[i][1][0]-1][V2[j][1][0]-1] + time_matrix[V2[i][1][1]-1][V2[j][1][1]-1]) , (time_matrix[V2[i][1][0]-1][V2[j][1][1]-1]+time_matrix[V2[i][1][1]-1][V2[j][1][0]-1]))
+	#symmetric wrt diagonal				
+	print "\nCOST MATRIX for TIME"
+	print T2
+	return T2
+
+
+
+D = distance_cost(roadmap_graph,adjacency_road,radiomap_graph,adjacency_radio)
+T = time_cost(roadmap_graph,adjacency_road,radiomap_graph,adjacency_radio)
 
 #TODO: G2
 """
