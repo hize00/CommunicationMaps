9455e00764780b4a4d580b923ac5cc808217ab14
Unknown <andrea3.cavalli@mail.polimi.it>
2017-09-19 10:28:41 +0200

    tentativo pose tf

---
+++ scripts/new_main_tf.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+
+from copy import deepcopy
+import math
+import os
+import pickle
+import random
+import sys
+import threading
+
+import rospy
+import tf
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseFeedback
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Point, Twist, Vector3, PoseWithCovarianceStamped, PoseStamped, Pose, Quaternion
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Path
+from std_srvs.srv import Empty
+from std_msgs.msg import Float32, Bool
+
+import communication
+from environment import Environment
+import exploration_strategies
+from GPmodel import GPmodel
+import utils
+from utils import conv_to_hash, eucl_dist
+from strategy.msg import GoalWithBackup, SignalData, RobotInfo, AllInfo, SignalMappingAction, \
+                               SignalMappingGoal, SignalMappingFeedback, SignalMappingResult
+from strategy.srv import GetSignalData, GetSignalDataResponse
+
+TIME_STUCK = 3.0
+TIME_AGAINST_WALL = 5.0
+SPEED = 0.45 # TODO It should depend on the settings of the planner.
+TIME_TOL_PERC = 0.04 # TODO It should depend on the settings of the planner and velocity.
+REPLAN_RATE = 10 # Frequency rate at which the leader is checking again the plan
+
+#TURTLEBOT CUSTOM
+MIN_SCAN_ANGLE_RAD_FRONT = -30.0*3.14/180.0
+MAX_SCAN_ANGLE_RAD_FRONT = 30.0*3.14/180.0
+
+#first random, max_var offices had 2
+MIN_FRONT_RANGE_DIST = 1.5 # TODO It should depend on the settings of the planner.
+MAX_NUM_ERRORS = 40
+PATH_DISC = 1 #m
+
+
+class GenericRobot(object):
+    def __init__(self, seed, robot_id, is_leader, sim, comm_range, map_filename, duration,
+                 log_filename, comm_dataset_filename, teammates_id, n_robots, ref_dist, resize_factor,
+                 errors_filename, client_topic='move_base'):
+
+        self.robot_id = robot_id
+        self.sim = sim
+        self.seed = seed
+        random.seed(seed + robot_id)
+        self.map_filename = map_filename
+        self.teammates_id = teammates_id
+        self.is_leader = is_leader
+        self.n_robots = n_robots
+
+        self.tol_dist = 2.5
+        self.errors_filename = errors_filename
+        self.error_count = 0
+
+        #for ending the mission
+        self.duration = rospy.Duration(duration)
+        self.mission_start_time = rospy.Time.now()
+
+        # for handling motion
+        self.client_topic = client_topic
+        self.goal_sent = False
+        self.client_motion = actionlib.SimpleActionClient(self.client_topic, MoveBaseAction)
+        self.client_motion.wait_for_server()
+        rospy.logdebug('initialized action exec')
+        self.clear_costmap_service = rospy.ServiceProxy('move_base_node/clear_costmaps', Empty)
+
+        # for communication
+        self.comm_range = comm_range
+        self.comm_module = communication.CommunicationModule(sim, seed, robot_id, n_robots, comm_range, ref_dist,
+                                                             map_filename, resize_factor)
+
+        # for logging
+        self.log_filename = log_filename
+        log_file = open(log_filename, "w")
+        log_file.close()
+        rospy.Timer(rospy.Duration(10), self.distance_logger)
+
+        self.comm_dataset_filename = comm_dataset_filename
+        log_dataset_file = open(comm_dataset_filename, "w")
+        log_dataset_file.close()
+
+
+        # estimated position
+        listener = tf.TransformListener()
+
+
+        cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist)
+        rate = rospy.Rate(10.0)
+        while not rospy.is_shutdown():
+            try:
+                (trans, rot) = listener.lookupTransform('/odom', '/base_pose_ground_truth', rospy.Time(0))
+            except (tf.LookupException,  tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+
+            # Get the 2-D position and orientation of the robot
+            x = trans[0]
+            y = trans[1]
+            euler_angles = euler_from_quaternion(rot)
+            theta = euler_angles[2]
+
+            rospy.loginfo('Position: %s,%s',x,y)
+            cmd = geometry_msgs.msg.Twist()
+            cmd.linear.x = linear
+            cmd.angular.z = angular
+            cmd_vel_publisher.publish(cmd)
+
+            rate.sleep()
+
+    def distance_logger(self, event):
+        f = open(self.log_filename, "a")
+        f.write(
+            'D ' + str((rospy.Time.now() - self.mission_start_time).secs) + ' ' + str(self.traveled_dist) + '\n')
+        f.close()
+
+        if ((rospy.Time.now() - self.mission_start_time) >= self.duration):
+            rospy.loginfo("Sending shutdown...")
+            os.system("pkill -f ros")
+
+    def go_to_pose(self, pos, quat):
+        self.goal_sent = True
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
+                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
+
+        # Start moving
+        self.client_motion.send_goal(goal)
+
+        # Allow TurtleBot up to 60 seconds to complete task
+        success = self.client_motion.wait_for_result(rospy.Duration(60))
+
+        result = False
+
+        if success:
+            # We made it!
+            result = True
+        else:
+            self.client_motion.cancel_goal()
+
+        self.goal_sent = False
+
+        #return result
+
+
+class Leader(GenericRobot):
+    def __init__(self, seed, robot_id, sim, comm_range, map_filename,
+                 duration, disc_method, disc, log_filename, teammates_id, n_robots, ref_dist,
+                 env_filename, comm_dataset_filename, resize_factor, tiling, errors_filename,
+                 communication_model):
+
+        rospy.loginfo(str(robot_id) + ' - Leader - starting!')
+        if communication_model is "":
+            self.filter_locations = False
+        else:
+            self.filter_locations = True
+        if not (os.path.exists(env_filename)):
+            print "Creating new environment."
+            f = open(env_filename, "wb")
+            self.env = Environment(map_filename, disc_method, disc, resize_factor, comm_range,communication_model)
+            pickle.dump(self.env, f)
+            f.close()
+        else:
+            f = open(env_filename, "rb")
+            self.env = pickle.load(f)
+            f.close()
+
+        super(Leader, self).__init__(seed, robot_id, True, sim, comm_range, map_filename,
+                                     duration, log_filename, comm_dataset_filename, teammates_id, n_robots,
+                                     ref_dist, resize_factor, errors_filename)
+
+        print 'created environment variable'
+        rospy.loginfo(str(robot_id) + ' - Created environment variable')
+
+
+        self.comm_map = GPmodel(self.env.dimX, self.env.dimY, comm_range, tiling, self.comm_module.comm_model, self.log_filename)
+        self.comm_maps = [] #for logging
+
+        #for sending commands to the follower
+        #the name of the server is the name of the robot itself
+        self.clients_signal = {}
+        for teammate_id in teammates_id:
+            self.clients_signal[teammate_id] = actionlib.SimpleActionClient('/robot_' + str(teammate_id) + '/main_robot', SignalMappingAction)
+            rospy.loginfo(str(self.robot_id) + ' - Leader - waiting for follower server ' + str(teammate_id))
+            self.clients_signal[teammate_id].wait_for_server()
+            rospy.loginfo(str(self.robot_id) + ' - Done.')
+
+
+
+class Follower(GenericRobot):
+    def __init__(self, seed, robot_id, sim, comm_range, map_filename, duration,
+                 log_filename, comm_dataset_filename, teammates_id, n_robots, ref_dist, env_filename,
+                 resize_factor, errors_filename):
+
+        rospy.loginfo(str(robot_id) + ' - Follower - starting!')
+        # Load Environment for follower to filter readings.
+        environment_not_loaded = True
+
+        while environment_not_loaded:
+            try:
+                f = open(env_filename, "rb")
+                self.env = pickle.load(f)
+                f.close()
+                environment_not_loaded = False
+            except:  # TODO specific exception.
+                rospy.logerr(str(robot_id) + " - Follower - Environment not loaded yet.")
+                rospy.sleep(1)
+
+        super(Follower, self).__init__(seed, robot_id, False, sim, comm_range, map_filename, duration,
+                                       log_filename, comm_dataset_filename, teammates_id, n_robots, ref_dist,
+                                       resize_factor, errors_filename)
+
+        self._action_name = rospy.get_name()
+
+        self._as = actionlib.SimpleActionServer(self._action_name, SignalMappingAction, auto_start=False)
+
+        print 'created environment variable'
+
+        rospy.loginfo(str(robot_id) + ' - Follower - created environment variable!')
+
+        self._as.start()
+
+
+if __name__ == '__main__':
+    rospy.init_node('robot')
+
+    robot_id = int(rospy.get_param('~id'))
+    sim = int(rospy.get_param('/sim'))
+    seed = int(rospy.get_param('/seed'))
+    ref_dist = int(rospy.get_param('/ref_dist'))
+    map_filename = rospy.get_param('/map_filename')
+    disc = float(rospy.get_param('/disc'))
+    comm_range = float(rospy.get_param('/range'))
+    disc_method = rospy.get_param('/disc_method')
+    duration = float(rospy.get_param('/duration'))
+    env_filename = rospy.get_param('/env_filename')
+
+    communication_model = rospy.get_param('/communication_model', "")
+    if communication_model is "":
+        env_filename = env_filename.replace(".dat","_" + str(int(comm_range)) + ".dat")
+    else:
+        env_filename = env_filename.replace(".dat","_" + str(int(comm_range)) + '_' + communication_model + ".dat")
+    print env_filename
+
+    teammates_id_temp = str(rospy.get_param('~teammates_id'))
+    is_leader = int(rospy.get_param('~is_leader'))
+    n_robots = int(rospy.get_param('/n_robots'))
+    resize_factor = float(rospy.get_param('/resize_factor'))
+    tiling = int(rospy.get_param('/tiling'))
+    log_folder = rospy.get_param('/log_folder')
+
+    temmates_id_temp = teammates_id_temp.split('-')
+    teammates_id = map(lambda x: int(x), temmates_id_temp)
+
+    env = (map_filename.split('/')[-1]).split('.')[0]
+    if communication_model is "":
+        log_filename = log_folder + str(seed) + '_' + env + '_' + str(robot_id) + '_' + \
+                       str(n_robots) + '_' + str(int(comm_range)) + '.log'
+    else:
+        log_filename = log_folder + str(seed) + '_' + env + '_' + str(robot_id) + '_' + \
+                       str(n_robots) + '_' + str(int(comm_range)) + '_' + communication_model + '.log'
+    if communication_model is "":
+        comm_dataset_filename = log_folder + str(seed) + '_' + env + '_' + str(robot_id) + \
+                                '_' + str(n_robots) + '_' + str(int(comm_range)) + '.dat'
+    else:
+        comm_dataset_filename = log_folder + str(seed) + '_' + env + '_' + str(robot_id) + \
+                                '_' + str(n_robots) + '_' + str(int(comm_range)) + '_' + communication_model + '.dat'
+
+    errors_filename = log_folder + 'errors.log'
+    print "Logging possible errors to: " + errors_filename
+
+    position = {'x': 11.50, 'y': 12.50}
+    quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': 0.000, 'r4': 1.000}
+
+    if is_leader:
+        lead = Leader(seed, robot_id, sim, comm_range, map_filename, duration,
+                      disc_method, disc, log_filename, teammates_id, n_robots, ref_dist, env_filename,
+                      comm_dataset_filename, resize_factor, tiling, errors_filename, communication_model)
+        rospy.loginfo("Leader goes to (%s, %s) position", position['x'], position['y'])
+        lead.go_to_pose(position,quaternion)
+    else:
+        foll = Follower(seed, robot_id, sim, comm_range, map_filename, duration, log_filename,
+                        comm_dataset_filename, teammates_id, n_robots, ref_dist, env_filename,
+                        resize_factor, errors_filename)
+        rospy.loginfo("Follower goes to (%s, %s) position", position['x'], position['y'])
+        foll.go_to_pose(position, quaternion)
+
