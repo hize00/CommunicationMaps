75e6b41ec1f8dca0f5432875b2beb0794319e30a
Boris_il_forte <boris.ilpossente@hotmail.it>
2017-10-23 11:38:31 +0000

    First logs

---
+++ scripts/ALGORITHM_Carlo/exp/data_offices1_500/datfiles/resTk3_500.py
@@ -0,0 +1,32 @@
+DATFILE : offices1_3r_500.dat
+ENVIRONMENT : office
+ALGORITHM : KUMAR3
+RANGE : _500
+STARTING_POS : 42
+N_ROBOTS : 3
+KUMAR3 T / cardinality : 4698.0
+EXECUTION TIME KUMAR3: 964.191273212 seconds
+
+-------------------------------------------------------------------
+DATFILE : offices1_3r_500.dat
+ENVIRONMENT : office
+ALGORITHM : KUMAR3
+RANGE : _500
+STARTING_POS : 42
+N_ROBOTS : 3
+KUMAR3 T / heuristic : 4698.0
+EXECUTION TIME KUMAR3: 963.093266964 seconds
+
+-------------------------------------------------------------------
+DATFILE : offices1_3r_500.dat
+ENVIRONMENT : office
+ALGORITHM : KUMAR3
+RANGE : _500
+STARTING_POS : 42
+N_ROBOTS : 3
+KUMAR3 T / objective : 5481.0
+EXECUTION TIME KUMAR3: 2295.240834 seconds
+
+-------------------------------------------------------------------
+MAP: offices1_
+DATE: 2017-10-23_10-17-01
