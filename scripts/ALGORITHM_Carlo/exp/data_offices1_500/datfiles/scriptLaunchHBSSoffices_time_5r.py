692cee9e0c9a675445bc3da9de0cac89716db488
hize00 <carlo.fanton92@gmail.com>
2017-11-09 12:37:56 +0100

    script 500

---
+++ scripts/ALGORITHM_Carlo/exp/data_offices1_500/datfiles/scriptLaunchHBSSoffices_time_5r.py
@@ -0,0 +1,29 @@
+
+import os
+import sys
+import datetime
+import multiprocessing
+
+ENVIRONMENT = 'offices1_'
+
+RANGE = 500
+
+MIN_ROBOT = 2
+MAX_ROBOT = 10
+nrob = 5
+ALGORITHM = ['HBSS']
+TAU_LIST = [3,5,7,9]
+OBJECTIVE = 'time'
+
+LAUNCH_DAT_FILES = []
+#LAUNCH_PY_FILES = []
+
+name_of_file = ENVIRONMENT + str(nrob) + "r_" + str(RANGE) + ".dat"
+
+
+for tau in TAU_LIST:
+	os.system("python " + "HBSS" + str(tau) + ".py" + " " + name_of_file + " " + OBJECTIVE + " > resultsT_hbss_" + ENVIRONMENT + str(RANGE) + "_tau" + str(tau) + "_" + str(nrob) + "r.txt" )
+	#print("python " + "HBSS" + str(tau) + ".py" + " " + name_of_file + " " + OBJECTIVE + " > resultsT_hbss_" + ENVIRONMENT + str(RANGE) + "_tau" + str(tau) + "_" + str(nrob) + "r.txt" )
+
+
+#"python HBSS3 filedati distance > resultsD_hbss_bwopen0_100_tau3_4r.txt"
